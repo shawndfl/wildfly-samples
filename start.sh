9fe830b181e0f0d2b139fbed2107c2bdf26d9dc3
shawndfl <shawnd.android@gmail.com>
2017-04-21 17:32:19 -0400

    Got JBoss running.

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-$WF_HOME/bin/standalone.sh --properties=./jboss.properties
+# Starts wildfly in standalone mode. need to add the log path here
+# or it will fail.
+$WF_HOME/bin/standalone.sh --properties=./jboss.properties -Djboss.server.log.dir=./server/log
