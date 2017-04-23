13d3739879919257e16a199ce84478e807934dba
shawndfl <shawnd.android@gmail.com>
2017-04-22 22:45:50 -0400

    Got hello :) working.

--- backend/src/main/java/com/sd/samples/backend/MainController.java
+++ backend/src/main/java/com/sd/samples/backend/MainController.java
@@ -1,5 +1,26 @@
 package com.sd.samples.backend;
 
-public class MainController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController
+{
+   protected final Log logger = LogFactory.getLog(getClass());
+   
+   public ModelAndView HomePage(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException
+   {
+      logger.info("Returning hello view");
+
+      return new ModelAndView("index");
+   }
 }
