85c8ef51edbd3f5026fce59011ba8e963c867adf
shawndfl <shawnd.android@gmail.com>
2017-05-13 09:12:09 -0400

    Copy hello sample for web service.

---
+++ samples/web-service/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# This script will start wildfly in standalone mode and use the properties found in
+# jobss.properties
+# to stop the wildfly server simple hit ^C
+#
+
+#
+# Make a deployment directory 
+#
+mkdir -p server/deployments
+
+#
+# Start wildfly. Make sure $WF_HOME/bin is in your PATH environment variable.
+#
+standalone.sh --properties=./jboss.properties -Djboss.server.log.dir=./server/log
